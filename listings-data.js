58efb4cba824b98b3ef3403c9bed1620378377b6
Ky Wildermuth <kywildermuth@gmail.com>
2022-04-05 16:48:20 -0500

    add listing data

---
+++ listings-data.js
@@ -0,0 +1,24 @@
+const productListings = [
+  {
+    id: 1,
+    name: "Industrial Microwave",
+    price: 10000,
+    quantity: 3,
+    description: "This is a microwave that is used in the manufacturing industry. It is really big.",
+    image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    name: "Broken Smartphones",
+    price: 3,
+    quantity: 100,
+    description: "I have a bunch of old broken smartphones that I don't want to own anymore.",
+  },
+  {
+    id: 3,
+    name: "Empty soda bottles",
+    price: 0.5,
+    quantity: 450,
+    description: "Does anybody want to buy some empty soda bottles?",
+  }
+]
